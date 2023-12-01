2241a4a18f4731909542bc394d248c9852cb7380
beechang <dbsckd92@naver.com>
2023-12-02 03:56:08 +0900

    update interface PlantDataSource

--- shared/src/commonMain/kotlin/com/kmm/moonflower/core/local/PlantDataSource.kt
+++ shared/src/commonMain/kotlin/com/kmm/moonflower/core/local/PlantDataSource.kt
@@ -1,8 +1,12 @@
 package com.kmm.moonflower.core.local
 
 interface PlantDataSource {
+
     suspend fun insertOrReplacePlants(plants: List<Plant>)
+
     suspend fun getAllPlants(): List<Plant>
+
     suspend fun getPlantById(id: String): Plant?
+
     suspend fun getPlantsWithGrowZoneNumber(growZoneNumber: Int): List<Plant>
 }
