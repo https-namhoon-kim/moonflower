1b601698df6dfc7fa3886191465af9dccb57c00d
beechang <dbsckd92@naver.com>
2023-12-01 14:45:00 +0900

    add Plant DataClass

---
+++ shared/src/commonMain/kotlin/com/kmm/moonflower/core/local/Plant.kt
@@ -0,0 +1,11 @@
+package com.kmm.moonflower.core.local
+
+data class Plant(
+    val plantId : String ,
+    val name : String,
+    val description : String ,
+    val growZoneNumber : Int ,
+    val wateringInterval :  Int ,
+    val imageUrl : String ,
+)
+
