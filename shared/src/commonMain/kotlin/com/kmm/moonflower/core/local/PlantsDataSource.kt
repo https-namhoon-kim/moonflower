9aa71132ad723dfb8e83289ac286145a12e03a2e
beechang <dbsckd92@naver.com>
2023-12-01 16:02:45 +0900

    add local datasource

---
+++ shared/src/commonMain/kotlin/com/kmm/moonflower/core/local/PlantsDataSource.kt
@@ -0,0 +1,65 @@
+package com.kmm.moonflower.core.local
+
+import com.example.moonflowerproto.database.AppDatabase
+import database.AppDatabaseQueries
+import database.Plants
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * @param dispatcher 안드로이드 io 디스패처 사용
+ */
+class PlantsDataSource(
+    private val database: AppDatabase,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Default
+) {
+
+    private val query : AppDatabaseQueries = database.appDatabaseQueries
+
+    suspend fun insertOrReplacePlants(plants: List<Plant>) {
+        withContext(dispatcher) {
+            query.run {
+                transaction {
+                    plants.forEach { plant ->
+                        insertPlantsAll(
+                            id = plant.plantId,
+                            name = plant.name,
+                            description = plant.description,
+                            growZoneNumber = plant.growZoneNumber.toLong(),
+                            wateringInterval = plant.growZoneNumber.toLong(),
+                            imageUrl = plant.imageUrl
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    suspend fun getAllPlants(): List<Plant> = withContext(dispatcher) {
+        query.getPlants()
+            .executeAsList()
+            .map { it.toPlantModel() }
+    }
+
+    suspend fun getPlantById(id: String): Plant? = withContext(dispatcher) {
+        query.getPlant(id)
+            .executeAsOneOrNull()?.toPlantModel()
+    }
+
+    suspend fun getPlantsWithGrowZoneNumber(growZoneNumber: Int): List<Plant> =
+        withContext(dispatcher) {
+            query.getPlantsWithGrowZoneNumber(growZoneNumber.toLong())
+                .executeAsList()
+                .map { it.toPlantModel() }
+        }
+
+    private fun Plants.toPlantModel() = Plant(
+        plantId = this.id,
+        name = this.name,
+        description = this.description,
+        growZoneNumber = this.growZoneNumber.toInt(),
+        wateringInterval = this.wateringInterval.toInt(),
+        imageUrl = this.imageUrl
+    )
+}
