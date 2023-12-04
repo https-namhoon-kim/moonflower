fd1df95f21fa77c3f5f487266c79edda4e11c2ad
beechang <dbsckd92@naver.com>
2023-12-05 01:54:03 +0900

    remove debug code for ios

--- shared/src/commonMain/kotlin/com/kmm/moonflower/core/resources/FileResourceReaderWithCompose.kt
+++ shared/src/commonMain/kotlin/com/kmm/moonflower/core/resources/FileResourceReaderWithCompose.kt
@@ -1,8 +1,5 @@
 package com.kmm.moonflower.core.resources
 
-
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.resource
 
@@ -12,16 +9,4 @@ class FileResourceReaderWithCompose : FileResourceReader {
     // https://stackoverflow.com/questions/75498653/sharing-resources-json-image-text-from-a-kotlin-multiplatform-module
     override suspend fun getJsonFileToString(fileName: String) =
         resource(fileName).readBytes().decodeToString()
-
-    fun testAsync(filename: String, completionHandler: (String?, Throwable?) -> Unit) {
-        val scope = MainScope()
-        scope.launch {
-            try {
-                val result = getJsonFileToString(filename)
-                completionHandler(result, null)
-            } catch (e: Throwable) {
-                completionHandler(null, e)
-            }
-        }
-    }
 }
