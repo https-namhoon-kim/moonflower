ea680372c16a900105f16a8bd7dd8a2353efff4b
beechang <dbsckd92@naver.com>
2024-01-01 00:50:41 +0900

    add GardenPlantingLocalDataSource.getPlantedGardens and mapper

--- shared/src/commonMain/kotlin/com/kmm/moonflower/feature/garden/data/local/GardenPlantingLocalDataSource.kt
+++ shared/src/commonMain/kotlin/com/kmm/moonflower/feature/garden/data/local/GardenPlantingLocalDataSource.kt
@@ -1,12 +1,15 @@
 package com.kmm.moonflower.feature.garden.data.local
 
 import com.kmm.moonflower.feature.garden.domain.vo.Garden
+import com.kmm.moonflower.feature.garden.domain.vo.PlantAndGardenPlantings
 
 interface GardenPlantingLocalDataSource {
 
     suspend fun getGardenPlanting(): List<Garden>
 
     suspend fun isExistPlantInGardenPlanting(plantId: String): Boolean
+
+    suspend fun getPlantedGardens(): List<PlantAndGardenPlantings>
 
     suspend fun insertGardenPlanting(plantId: String, plantDate: Int, lastWateringDate: Int)
 
