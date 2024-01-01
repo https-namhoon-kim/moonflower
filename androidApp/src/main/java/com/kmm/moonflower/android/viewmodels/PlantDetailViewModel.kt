21a6a387d32a25f927f786bd96bdfb020b21e14e
beechang <dbsckd92@naver.com>
2024-01-01 22:41:12 +0900

    힐트뷰모델 제거, 라이브데이터로 교체

--- androidApp/src/main/java/com/kmm/moonflower/android/viewmodels/PlantDetailViewModel.kt
+++ androidApp/src/main/java/com/kmm/moonflower/android/viewmodels/PlantDetailViewModel.kt
@@ -20,37 +20,53 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
-import com.kmm.moonflower.android.BuildConfig
-import com.kmm.moonflower.android.data.GardenPlantingRepository
-import com.kmm.moonflower.android.data.PlantRepository
-import dagger.hilt.android.lifecycle.HiltViewModel
+import com.kmm.moonflower.di.GardenPlantingFeatureModule
+import com.kmm.moonflower.di.PlantFeatureModule
+import com.kmm.moonflower.feature.garden.domain.repository.GardenPlantingRepository
+import com.kmm.moonflower.feature.plant.domain.repository.PlantRepository
+import com.kmm.moonflower.feature.plant.domain.vo.Plant
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
  * The ViewModel used in [PlantDetailFragment].
  */
-@HiltViewModel
 class PlantDetailViewModel @Inject constructor(
-    savedStateHandle: SavedStateHandle,
-    plantRepository: PlantRepository,
-    private val gardenPlantingRepository: GardenPlantingRepository,
+    savedStateHandle:SavedStateHandle,
+    private val plantRepository: PlantRepository = PlantFeatureModule.providePlantRepository(),
+    private val gardenPlantingRepository: GardenPlantingRepository = GardenPlantingFeatureModule.provideGardenPlantingRepository()
 ) : ViewModel() {
 
     val plantId: String = savedStateHandle.get<String>(PLANT_ID_SAVED_STATE_KEY)!!
 
-    val isPlanted = gardenPlantingRepository.isPlanted(plantId).asLiveData()
-    val plant = plantRepository.getPlant(plantId).asLiveData()
+    private val _isPlanted = MutableLiveData(false)
+    val isPlanted: LiveData<Boolean>
+        get() = _isPlanted
+
+    private val _plant = MutableLiveData(Plant(plantId , "","",-1,-1,""))
+    val plant: LiveData<Plant>
+        get() = _plant
 
     private val _showSnackbar = MutableLiveData(false)
     val showSnackbar: LiveData<Boolean>
         get() = _showSnackbar
 
+    init {
+        viewModelScope.launch {
+            _plant.value = plantRepository.getPlantById(plantId)
+            _isPlanted.value = gardenPlantingRepository.isExistPlantInGardenPlanting(plantId)
+         }
+    }
+
     fun addPlantToGarden() {
         viewModelScope.launch {
-            gardenPlantingRepository.createGardenPlanting(plantId)
+            val time = System.currentTimeMillis()
+            gardenPlantingRepository.insertGardenPlanting(
+                plantId = plantId ,
+                plantDate = time ,
+                lastWateringDate = time ,
+            )
             _showSnackbar.value = true
         }
     }
@@ -65,3 +81,29 @@ class PlantDetailViewModel @Inject constructor(
         private const val PLANT_ID_SAVED_STATE_KEY = "plantId"
     }
 }
+
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            FeatherAndroidTasksTheme {
+//                // Create an instance of the ViewModel using viewModel function
+//                val viewModel: MyViewModel = remember {
+//                    viewModel(factory = MyViewModelFactory(savedStateHandle = savedStateHandle))
+//                }
+//
+//                // Use the viewModel in your Compose UI
+//                Greeting(name = viewModel.name)
+//
+//                // Other Compose components...
+//            }
+//        }
+//    }
+//}
+//
+//// ViewModel class with SavedStateHandle
+//class MyViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+//    // Example property in the ViewModel
+//    var name by savedStateHandle.supportedValue("name_key", "DefaultName")
+//        private set
+//}
